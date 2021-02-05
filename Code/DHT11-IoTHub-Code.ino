2fe8273b9987bb0a872077337898797a74470beb
Prasenjit Saha <prasenjit-s@hcl.com>
2021-02-05 11:21:25 +0530

    Update DHT11-IoTHub-Code.ino

--- Code/DHT11-IoTHub-Code.ino
+++ Code/DHT11-IoTHub-Code.ino
@@ -1,27 +1,21 @@
-//Azure IoT Hub + DHT11 + NodeMCU ESP8266
+//Azure IoT Hub + DHT11 + NodeMCU ESP8266 Experiment Done By Prasenjit Saha
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266HTTPClient.h>
 #include "DHT.h"
 
 // WiFi settings
-const char* ssid = "Prasenjit Saha";
-const char* password = "Prathama@123";
+const char* ssid = "YourWifiName";
+const char* password = "WifiPassword";
 
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
 
-// Azure Setting Event Hub 
-//const String AzureEventHubURI="https://{your-namespace}.servicebus.windows.net/{your-event-hub}/messages?timeout=60&api-version=2014-01"; //messages is fix and not the name of the access policy
-//const String AzureEventHubFingerPrint="";
-//const String AzureEventHubAuth="";
-
 //Azure IoT Hub
-//POST https://fully-qualified-iothubname.azure-devices.net/devices/{id}/messages/events?api-version=2020-03-13
-const String AzureIoTHubURI="https://myioteventhub.azure-devices.net/devices/esp8266/messages/events?api-version=2020-03-13"; //messages is fix and not the name of the access policy
-//openssl s_client -servername myioteventhub.azure-devices.net -connect myioteventhub.azure-devices.net:443 | openssl x509 -fingerprint -noout //Remove :
-const String AzureIoTHubFingerPrint="9C:46:08:5D:55:5C:F7:83:79:0A:03:BD:DE:F4:54:F2:C9:E6:FF:9C"; //"9C46085D555CF783790A03BDDEF454F2C9E6FF9C";
+const String AzureIoTHubURI="https://{iothubname}.azure-devices.net/devices/{id}/messages/events?api-version=2020-03-13"; 
+//openssl s_client -servername myioteventhub.azure-devices.net -connect myioteventhub.azure-devices.net:443 | openssl x509 -fingerprint -noout //
+const String AzureIoTHubFingerPrint="{YourGeneratedFingerPrint}"; 
 //az iot hub generate-sas-token --device-id MyTestDevice --hub-name {YourIoTHubName} 
-const String AzureIoTHubAuth="SharedAccessSignature sr=myioteventhub.azure-devices.net%2Fdevices%2Fesp8266&sig=yfFJLvULbwm1pJ0H18VyjSuAdBmrJg4PfWv8k0B8X4Y%3D&se=1612447403";
+const String AzureIoTHubAuth="{SAS Token}";
 
 #define DHTTYPE DHT11  
 
@@ -52,9 +46,6 @@ void setup() {
 void loop() {
   // Wait a few seconds between measurements.
   delay(5000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
